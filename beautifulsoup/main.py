d139ad2e28344913c1e91831d688050824ed50de
Daniel Buecheler <dabch@posteo.de>
2021-12-07 18:20:41 +0100

    Add plotting of Vijaita Singh data

--- beautifulsoup/main.py
+++ beautifulsoup/main.py
@@ -11,14 +11,25 @@ def main():
     # print(post_data)
 
     post_data = pd.read_json('vieta_2.json', convert_dates=['time'])
+    post_data.set_index('time', inplace=True)
 
     # date_index = pd.to_datetime(post_data['time'])
 
-    g = post_data.groupby(pd.Grouper(key='time', freq='M'))
+    g = post_data[post_data.index >= datetime(2021, 1, 1)]
+    g = g['title'].groupby(pd.Grouper(freq='M'))
+    # g.index.map(lambda dt: dt.month)
     counts = g.count()
-
-    counts.unstack().plot(kind='bar')
+    counts.index = counts.index.map(lambda dt: dt.strftime('%b'))
     print(counts)
+
+    plt.title('Posts by Vijaita Sing on thehindu.com (2021')
+    counts.plot(kind='bar', xlabel='Month in 2021', ylabel='# of posts')
+    #
+    # x = g['title']
+    # x.plot()
+    #
+    # counts['time', 'title'].plot()
+    # print(counts)
 
     # for name, group in times:
     #     print(name)
@@ -26,7 +37,7 @@ def main():
     #
     # sums = times.sum()
     # print(post_data)
-    # plt.show()
+    plt.show()
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
