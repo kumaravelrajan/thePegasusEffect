dc2e4d1c62170b25e79b3bb0d1f381f2a3f7b0d8
Daniel Buecheler <dabch@posteo.de>
2021-12-07 16:04:35 +0100

    Add parsing for Vijaita and Nihalsing

--- beautifulsoup/main.py
+++ beautifulsoup/main.py
@@ -1,16 +1,39 @@
 from szabolcs_panyi_direkt32_hu import parse_all as parse_szabolcs
+from rathod_nihalsing_the_leaflet import parse_all as parse_rathod
+from singh_vieta_the_hindu import parse_all as parse_vieta
 from matplotlib import pyplot as plt
+from datetime import datetime, date
+import pandas as pd
 
 def main():
-    post_data = parse_szabolcs()
-    times = list(map(lambda post: post['time'], post_data))
+    # post_data = parse_vieta()
+    #
+    # print(post_data)
 
+    post_data = pd.read_json('vieta_2.json', convert_dates=['time'])
 
-    fig, ax = plt.subplots()
-    plt.xticks(rotation=45)
-    ax.eventplot(times, orientation='horizontal')
+    # date_index = pd.to_datetime(post_data['time'])
 
-    plt.show()
+    g = post_data.groupby(pd.Grouper(key='time', freq='M'))
+    counts = g.count()
+
+    counts.unstack().plot(kind='bar')
+    print(counts)
+
+    # for name, group in times:
+    #     print(name)
+    #     print(group.sum())
+    #
+    # sums = times.sum()
+    # print(post_data)
+    # plt.show()
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
 
 if __name__ == '__main__':
     main()
