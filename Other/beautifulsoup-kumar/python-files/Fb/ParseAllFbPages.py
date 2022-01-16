dbbe7a8572352ef2bce47b77bed4bf085920c808
Kumaravel Rajan <kumaravel.rajan@tum.de>
2022-01-16 21:38:35 +0100

    1. Changed twitter json file formatting. \n2. Added missing fb json files.

--- Other/beautifulsoup-kumar/python-files/Fb/ParseAllFbPages.py
+++ Other/beautifulsoup-kumar/python-files/Fb/ParseAllFbPages.py
@@ -13,42 +13,52 @@ import os
 os.system('clear')
 listForJson = []
 
-#todo - Change journo_name and page name!
-journo_name = "Carmen-Aristegui"
+dict_Name_Fb = {
+    'icembrero': 'ignacia_cembrero',
+    'ali.amar.585559' : 'ali_amar',
+    'ricardomraphael' : 'ricardo_raphael',
+    'iftikhar.gilani' : 'iftikar_ghilani',
+    'cesur.sumerinli' : 'jasur_sumerinli',
+    'SmitaSharmaJournalist' : 'smita_sharma',
+    'alejandro.sicairosrivas' : 'alejandro_sicairos',
+    'maria.moukrim' : 'maria_moukrim'
+}
 
 # Scrape Facebook for posts
 temporary_banned_count = 0
 dateExceed = 0
 startDateExceedCalculation = False
-try:
-    for post in get_posts('CarmenAristeguiOficial', pages=2000 , options={"allow_extra_requests": False, "posts_per_page": 200},  
-    cookies="/mnt/c/Users/kumar/Desktop/TUM/Seminar/pegasus_cybercrime_seminar_latest/beautifulsoup-kumar/python-files/Fb/cookies.json"):
-        try:
-            ctDate = parser.parse(post['time'].strftime("%Y-%m-%d"))
-            if((parser.parse(post['time'].strftime("%Y-%m-%d")) - parser.parse("2016-12-31")).days > 0):
-                listForJson.append({"title": post['post_text'], "time": post['time'].isoformat(), "author": journo_name, "url": post['post_url']})
 
-                if(startDateExceedCalculation):
-                    startDateExceedCalculation = False
-                    dateExceed = 0
-            else:
-                # When post date before 2019 dec 31 we come here.Then, check if genuinely date preceeded or a shared post has a prior date.
-                # startDateExceedCalculation set to true here. 
-                if(not startDateExceedCalculation):
-                    startDateExceedCalculation = True
+for index, dictKey in enumerate(dict_Name_Fb):
+    try:
+        for post in get_posts(dictKey, pages=2000 , options={"allow_extra_requests": False, "posts_per_page": 200},  
+        cookies="/mnt/c/Users/kumar/Desktop/TUM/Seminar/pegasus_cybercrime_seminar_latest/Other/beautifulsoup-kumar/python-files/Fb/cookies.json"):
+            try:
+                ctDate = parser.parse(post['time'].strftime("%Y-%m-%d"))
+                if((parser.parse(post['time'].strftime("%Y-%m-%d")) - parser.parse("2019-12-31")).days > 0):
+                    listForJson.append({"title": post['post_text'], "time": post['time'].isoformat(), "author": dict_Name_Fb[dictKey], "url": post['post_url']})
 
-                # 
-                if(startDateExceedCalculation):
-                    dateExceed += 1
-                if(dateExceed > 5):
-                    break
-        except:
-            continue
+                    if(startDateExceedCalculation):
+                        startDateExceedCalculation = False
+                        dateExceed = 0
+                else:
+                    # When post date before 2019 dec 31 we come here.Then, check if genuinely date preceeded or a shared post has a prior date.
+                    # startDateExceedCalculation set to true here. 
+                    if(not startDateExceedCalculation):
+                        startDateExceedCalculation = True
 
-    with open(f"/mnt/c/Users/kumar/Desktop/TUM/Seminar/pegasus_cybercrime_seminar_latest/beautifulsoup-kumar/json-files/Fb/{journo_name}-fb.json", "w") as f:
-        f.write(json.dumps(listForJson, indent=4, ensure_ascii=False))
-        f.flush()
-except exceptions.TemporarilyBanned as e:
-    temporary_banned_count += 1
-    sleep_secs = 600 * temporary_banned_count
-    time.sleep(sleep_secs)
+                    # 
+                    if(startDateExceedCalculation):
+                        dateExceed += 1
+                    if(dateExceed > 5):
+                        break
+            except:
+                continue
+
+        with open(f"/mnt/c/Users/kumar/Desktop/TUM/Seminar/pegasus_cybercrime_seminar_latest/json_files/" + dict_Name_Fb[dictKey] + '_facebook.json', "w") as f:
+            f.write(json.dumps(listForJson, indent=4, ensure_ascii=False))
+            f.flush()
+    except exceptions.TemporarilyBanned as e:
+        temporary_banned_count += 1
+        sleep_secs = 600 * temporary_banned_count
+        time.sleep(sleep_secs)
